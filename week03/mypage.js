d63a1848e08ce12dfab3a883fb5ae82d05e7951a
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-03-28 16:38:23 +0900

    feat: mypage.js에 아이디 일치 확인 기능 추가

--- week03/mypage.js
+++ week03/mypage.js
@@ -1,7 +1,9 @@
 function checkEdit() {
-  const id = prompt("아이디를 입력하세요.");
+  const savedId = localStorage.getItem("userId");
+  const inputId = prompt("아이디를 입력하세요.");
 
-  if (id === "sep") {
+  if (inputId === savedId) {
+    alert("✅ 수정 권한이 확인되었습니다!");
   } else {
     alert("❌ 수정 권한이 없습니다!");
   }
