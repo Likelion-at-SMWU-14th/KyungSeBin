b995411677f7a07e23e35f701db14cd22457ce84
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-03-31 19:56:02 +0900

    Chore: static 폴더 및 파일 생성

---
+++ week04/static/js/signup.js
@@ -0,0 +1,14 @@
+function checkPassword() {
+  const pw1 = document.getElementById("password1").value;
+  const pw2 = document.getElementById("password2").value;
+
+  if (pw1 !== pw2) {
+    alert("비밀번호가 달라요! 다시 확인해주세요 🦁");
+    return false;
+  }
+
+  const username = document.signup.username.value;
+  localStorage.setItem("userId", username);
+  
+  return true;
+}
