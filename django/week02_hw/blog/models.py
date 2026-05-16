0930908e3cce576fc5ccbf5ba6167503aa34536f
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-05-17 00:13:24 +0900

    Feat: FBV 및 CBV 작성

--- django/week02_hw/blog/models.py
+++ django/week02_hw/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Diary(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    mood = models.CharField(max_length=20, default='😊')
+
+    def __str__(self):
+        return self.title
