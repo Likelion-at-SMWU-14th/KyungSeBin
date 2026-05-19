d50f2b1ff4997b67c84c6604a21955bd07b11d39
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-05-19 20:31:21 +0900

    Feat: 게시물 CRUD

--- django/week03/myinsta/urls.py
+++ django/week03/myinsta/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from posts.views import url_view,url_parameter_view,function_view,class_view,class_view2,home_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('', home_view), #'' : default url -> 화면 들어가자마자 우리가 만든 화면이 뜸 ( 기존 : 404 에러가 남 )
     path('posts/', include('posts.urls', namespace='posts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
