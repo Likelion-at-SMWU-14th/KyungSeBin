2a5ea60dde232b2e5bc9b022db3253cc91913200
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-06-23 19:59:22 +0900

    Feat: @api_view+query param 계산기 구현

---
+++ django/week04/posts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import PostModelViewSet
+
+app_name='posts'
+
+router=routers.DefaultRouter()
+router.register('', PostModelViewSet,basename='posts')
+
+urlpatterns=[
+    path('', include(router.urls)),
+]
+
