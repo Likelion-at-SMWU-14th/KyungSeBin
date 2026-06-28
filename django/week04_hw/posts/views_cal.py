49fae30e80e05ee14e93ee86556d775a06fe28d9
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-06-28 17:31:19 +0900

    Feat:@api_view+request body 계산기 구현

--- django/week04_hw/posts/views_cal.py
+++ django/week04_hw/posts/views_cal.py
@@ -27,3 +27,28 @@ def calculator_query(request):
     }
     
     return Response(data)
+
+@api_view(['POST'])
+def calculator_body(request):
+    data=request.data
+    num1=data.get('num1',0)
+    num2=data.get('num2',0)
+    op=data.get('op')
+    
+    if op=='+':
+        result=int(num1)+int(num2)
+    elif op=='-':
+        result=int(num1)-int(num2)
+    elif op=='*':
+        result=int(num1)*int(num2)
+    elif op=='/':
+        result=int(num1)/int(num2)
+    else:
+        result=0
+        
+    data={
+        'op':op,
+        'result':result,
+    }
+    
+    return Response(data)
