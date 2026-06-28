33090b5592d59e4666097be5dd5dd0ff9ef567fb
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-06-28 20:06:42 +0900

    Feat: CRUD views.py에 작성

--- django/week04_hw/accounts/views.py
+++ django/week04_hw/accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
+from .models import User
 
-# Create your views here.
+#회원가입(Create) -> /users/
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#회원정보 조회(Read) ->/users/<id>
+@api_view(['GET'])
+def get_user(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = UserSerializer(user)
+    return Response(serializer.data,status=status.HTTP_200_OK)
+
+#회원정보 수정(Update) -> /users/<id>
+@api_view(['PUT'])
+def update_user(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist: #사용자가 존재하지 않을때
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = UserSerializer(user, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#회원정보 삭제(Delete) -> /users/<id>
+@api_view(['DELETE'])
+def delete_user(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist: #사용자가 존재하지 않을때
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    user.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
