0e9522f8ef785008b8a226ae54e40614c17c47b2
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-06-30 20:23:20 +0900

    Feat: 사용자 인증 구현

--- django/week05/myinsta/settings.py
+++ django/week05/myinsta/settings.py
@@ -40,7 +40,17 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'posts',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
