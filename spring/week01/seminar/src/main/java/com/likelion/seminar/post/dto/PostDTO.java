a5eef1b87d1b11a6feef344ce07661871a1550c3
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-07-21 19:49:36 +0900

    Feat:Post 생성 기능 구현

--- spring/week01/seminar/src/main/java/com/likelion/seminar/post/dto/PostDTO.java
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/dto/PostDTO.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class postDTO {
+public class PostDTO {
 
     private String title;
 
---
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/service/PostService.java
@@ -0,0 +1,19 @@
+package com.likelion.seminar.post.service;
+
+import com.likelion.seminar.post.dto.PostDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class PostService {
+
+    private final List<PostDTO> postDTOList;
+
+    public void createPost(PostDTO postDTO){
+        this.postDTOList.add(postDTO);
+    }
+}
