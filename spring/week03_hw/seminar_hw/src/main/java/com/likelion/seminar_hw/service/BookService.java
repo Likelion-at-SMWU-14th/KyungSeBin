6cee78e6933208b507cab95367623dfbb7e9393f
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-08-09 20:39:26 +0900

    Feat: 도서 등록, 조회 서비스 구현

--- spring/week03_hw/seminar_hw/src/main/java/com/likelion/seminar_hw/service/BookService.java
+++ spring/week03_hw/seminar_hw/src/main/java/com/likelion/seminar_hw/service/BookService.java
@@ -1,4 +1,22 @@
 package com.likelion.seminar_hw.service;
 
+import com.likelion.seminar_hw.model.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class BookService {
+
+    private final List<Book> books = new ArrayList<>();
+
+    public Book addBook(Book book) {
+        books.add(book);
+        return book;
+    }
+
+    public List<Book> findAll() {
+        return books;
+    }
 }
