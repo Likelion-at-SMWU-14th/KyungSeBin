0380b4974f2d041f8b6262650f02078cc03f02c9
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-08-09 20:41:29 +0900

    Feat: MVC 방식-도서 등록 ë및 조회 구현

--- spring/week03_hw/seminar_hw/src/main/java/com/likelion/seminar_hw/controller/BookController.java
+++ spring/week03_hw/seminar_hw/src/main/java/com/likelion/seminar_hw/controller/BookController.java
@@ -1,4 +1,30 @@
 package com.likelion.seminar_hw.controller;
 
+import com.likelion.seminar_hw.model.Book;
+import com.likelion.seminar_hw.service.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public String viewBooks(Model model) {
+        model.addAttribute("books", bookService.findAll());
+        return "books";
+    }
+
+    @PostMapping("/books")
+    public String addBook(Book book) {
+        bookService.addBook(book);
+        return "redirect:/books";
+    }
 }
