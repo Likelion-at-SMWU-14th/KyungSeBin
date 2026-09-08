a8ab372b311c407f009881fb5409998799a8ad87
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-09-08 20:14:41 +0900

    Feat: 일대일 매핑 ì실습

---
+++ spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Product.java
@@ -0,0 +1,33 @@
+package com.likelion.seminar.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Product extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long number;
+
+    //상품 이름
+    private String name;
+    //상품 가격
+    private Integer price;
+    //상품 재고
+    private Integer stock;
+
+    public Product(String name, Integer price){
+        this.name=name;
+        this.price=price;
+    }
+
+    @OneToOne(mappedBy="product")
+    @ToString.Exclude
+    private ProductDetail productDetail;
+}
