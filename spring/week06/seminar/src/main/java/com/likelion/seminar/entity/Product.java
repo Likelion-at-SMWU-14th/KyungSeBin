73ead4f6826e201a0ae96868a38e8f66a7d21dcf
junii2002 <kkyonssebb@sookmyung.ac.kr>
2026-09-08 20:32:17 +0900

    Feat:일대다,다대일 매핑 실습

--- spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Product.java
+++ spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Product.java
@@ -30,4 +30,12 @@ public class Product extends BaseEntity{
     @OneToOne(mappedBy="product")
     @ToString.Exclude
     private ProductDetail productDetail;
+
+    @ManyToOne
+    @JoinColumn(name="provider_id")
+    private Provider provider;
+
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
 }
